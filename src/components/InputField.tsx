1caf25a92c8839556383a248fc870cb9cb033e18
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-03 13:12:26 -0400

    pass props with interface

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./styles.css";
+import { EventType } from "@testing-library/react";
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="input" placeholder="enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
